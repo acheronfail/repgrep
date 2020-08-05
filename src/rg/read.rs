fae140bda9489dda86cb07e7e7fe7c8da7c2af65
acheronfail <acheronfail@gmail.com>
2020-08-05 22:51:00 +1000

    fix: handle case when rg returns no matches
    Fixes #24

--- src/rg/read.rs
+++ src/rg/read.rs
@@ -6,6 +6,8 @@ use anyhow::{anyhow, Result};
 use crate::rg::de::RgMessage;
 
 pub fn read_messages<R: Read>(rdr: R) -> Result<VecDeque<RgMessage>> {
+    let mut saw_match_message = false;
+
     let mut rg_messages: VecDeque<RgMessage> = VecDeque::new();
     let reader = BufReader::new(rdr);
     for (i, line) in reader.lines().enumerate() {
@@ -15,10 +17,20 @@ pub fn read_messages<R: Read>(rdr: R) -> Result<VecDeque<RgMessage>> {
             let _ = io::stdout().flush();
         }
 
-        rg_messages.push_back(
-            serde_json::from_str(&line?).map_err(|e| anyhow!("Failed to parse JSON: {}", e))?,
-        );
+        let rg_msg: RgMessage =
+            serde_json::from_str(&line?).map_err(|e| anyhow!("Failed to parse JSON: {}", e))?;
+
+        if !saw_match_message && matches!(rg_msg, RgMessage::Match { .. }) {
+            saw_match_message = true;
+        }
+
+        rg_messages.push_back(rg_msg);
     }
 
-    Ok(rg_messages)
+    // We expect at least one message.
+    if !saw_match_message {
+        Err(anyhow!("No matches returned from rg!"))
+    } else {
+        Ok(rg_messages)
+    }
 }

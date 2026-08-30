fae161b7f95cb34df66dce1459f3fb2ce2d85799
acheronfail <acheronfail@gmail.com>
2026-08-30 19:16:16 +0930

    fix cli argument parsing for ripgrep flags
    Fixes #116

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -26,8 +26,13 @@ EXAMPLES:
     There are different ways to invoke {bin}:
 
     1: {bin} <RG_ARGS>...
-        In this way, {bin} is a thin wrapper for rg and you may pass any rg arguments
-        you wish. {bin} will take care of forwarding them to rg and spawn it for you.
+        In this way, {bin} is a thin wrapper for rg and you may pass its search arguments.
+        {bin} will take care of forwarding them to rg and spawn it for you. Output modes
+        that don't produce search matches, such as `--count` and `--files`, aren't supported.
+
+        When using a value-taking option introduced by a newer version of rg, pass its
+        value in the `--newflag=somevalue` format. This lets {bin} distinguish the option's
+        value from the search pattern before that new option is known to {bin}.
 
         {bin} "foo"
             Find and replace all occurrences of "foo".
@@ -141,33 +146,23 @@ impl RgArgs {
         RgArgs::parse_rg_args_impl(Parser::from_env())
     }
 
-    fn parse_rg_unknown_arg(
+    fn parse_rg_other_arg(
         parser: &mut Parser,
         name: impl AsRef<str>,
         short: bool,
+        takes_value: bool,
     ) -> Result<String> {
         use lexopt::prelude::*;
 
         let name = name.as_ref();
-        let next_is_flag = parser
-            .try_raw_args()
-            .map(|raw_args| {
-                raw_args
-                    .peek()
-                    .and_then(|next| next.to_str())
-                    // if there's no next value, this must be a flag
-                    // if there is a next value, see if it looks like a flag
-                    .map_or(true, |s| s.starts_with('-'))
-            })
-            // if `try_raw_args` failed, then we're passing something with an optional
-            // value, so that's not a flag
-            .unwrap_or(false);
+        // `try_raw_args` fails when a value is attached to the current argument, such as
+        // `--sort=path`, `-C2` or `-in`. Preserve attached values and short flag groups while only
+        // consuming the next argument for options that ripgrep defines as taking a value.
+        let has_attached_value = parser.try_raw_args().is_none();
 
         let dash = if short { "-" } else { "--" };
         let equals = if short { "" } else { "=" };
-        Ok(if next_is_flag {
-            format!("{dash}{flag}", dash = dash, flag = name)
-        } else {
+        Ok(if takes_value || has_attached_value {
             format!(
                 "{dash}{flag}{equals}{value}",
                 dash = dash,
@@ -175,7 +170,57 @@ impl RgArgs {
                 equals = equals,
                 value = parser.value()?.string()?
             )
+        } else {
+            format!("{dash}{flag}", dash = dash, flag = name)
         })
+    }
+
+    fn rg_short_arg_takes_value(name: char) -> bool {
+        matches!(
+            name,
+            'A' | 'B' | 'C' | 'd' | 'E' | 'e' | 'f' | 'g' | 'j' | 'm' | 'M' | 'r' | 't' | 'T'
+        )
+    }
+
+    fn rg_long_arg_takes_value(name: &str) -> bool {
+        matches!(
+            name,
+            "after-context"
+                | "before-context"
+                | "color"
+                | "colors"
+                | "context"
+                | "context-separator"
+                | "dfa-size-limit"
+                | "encoding"
+                | "engine"
+                | "field-context-separator"
+                | "field-match-separator"
+                | "file"
+                | "generate"
+                | "glob"
+                | "hostname-bin"
+                | "hyperlink-format"
+                | "iglob"
+                | "ignore-file"
+                | "max-columns"
+                | "max-count"
+                | "max-depth"
+                | "max-filesize"
+                | "path-separator"
+                | "pre"
+                | "pre-glob"
+                | "regex-size-limit"
+                | "regexp"
+                | "replace"
+                | "sort"
+                | "sortr"
+                | "threads"
+                | "type"
+                | "type-add"
+                | "type-clear"
+                | "type-not"
+        )
     }
 
     // TODO: this implementation assumes UTF-8 (via `String`) for all arguments, but in reality it
@@ -229,13 +274,25 @@ impl RgArgs {
                     fixed_strings = false;
                 }
 
+                // These ripgrep modes either don't emit search results or are incompatible with
+                // the JSON match stream that rgr requires.
+                Short('c') | Long("count") | Long("count-matches") => {
+                    bail!("ripgrep count output modes are not supported in rgr")
+                }
+                Short('l') | Long("files-with-matches") | Long("files-without-match") => {
+                    bail!("ripgrep file-list output modes are not supported in rgr")
+                }
+                Long("files") | Long("generate") | Long("pcre2-version") | Long("type-list") => {
+                    bail!("ripgrep special output modes are not supported in rgr")
+                }
+
                 // capture help to display our help
                 // also important to capture these since they make `rg` not output JSON!
                 Short('h') | Long("help") => {
                     print_help();
                     process::exit(0);
                 }
-                Short('v') | Long("version") => {
+                Short('V') | Long("version") => {
                     println!(
                         "{crate_name} {crate_version}",
                         crate_name = env!("CARGO_PKG_NAME"),
@@ -245,14 +302,21 @@ impl RgArgs {
                 }
 
                 // ripgrep: all other arguments and flags
-                Short(ch) => other_args.push(RgArgs::parse_rg_unknown_arg(
+                Short(ch) => other_args.push(RgArgs::parse_rg_other_arg(
                     &mut parser,
                     String::from(ch),
                     true,
+                    RgArgs::rg_short_arg_takes_value(ch),
                 )?),
                 Long(name) => {
                     let name = name.to_owned();
-                    other_args.push(RgArgs::parse_rg_unknown_arg(&mut parser, name, false)?);
+                    let takes_value = RgArgs::rg_long_arg_takes_value(&name);
+                    other_args.push(RgArgs::parse_rg_other_arg(
+                        &mut parser,
+                        name,
+                        false,
+                        takes_value,
+                    )?);
                 }
                 Value(other) => other_args.push(other.string()?),
             }
@@ -315,9 +379,15 @@ mod tests {
         parse_pattern!["pattern", "--flag"];
     }
 
+    macro_rules! try_parse_rg {
+        [$($arg:expr$(,)?)*] => {
+            RgArgs::parse_rg_args_impl(Parser::from_iter(["rgr".to_string(), $($arg.into(),)*]))
+        };
+    }
+
     macro_rules! parse_rg {
         [$($arg:expr$(,)?)*] => {
-            RgArgs::parse_rg_args_impl(Parser::from_iter(["rgr".to_string(), $($arg.into(),)*])).unwrap()
+            try_parse_rg![$($arg),*].unwrap()
         };
     }
 
@@ -412,6 +482,184 @@ mod tests {
     }
 
     #[test]
+    fn rg_pcre2() {
+        let pattern = r"(\w+) \1";
+
+        let args = parse_rg!["--pcre2", pattern];
+        assert_eq!(args.patterns, [pattern]);
+        assert_eq!(args.other_args, ["--pcre2"]);
+        assert_eq!(args.rg_args(), ["--pcre2", r"--regexp=(\w+) \1"]);
+
+        let args = parse_rg!["-P", pattern];
+        assert_eq!(args.patterns, [pattern]);
+        assert_eq!(args.other_args, ["-P"]);
+        assert_eq!(args.rg_args(), ["-P", r"--regexp=(\w+) \1"]);
+
+        let args = parse_rg![pattern, "--pcre2", "--no-pcre2"];
+        assert_eq!(args.patterns, [pattern]);
+        assert_eq!(args.other_args, ["--pcre2", "--no-pcre2"]);
+        assert_eq!(
+            args.rg_args(),
+            ["--pcre2", "--no-pcre2", r"--regexp=(\w+) \1"]
+        );
+    }
+
+    #[test]
+    fn rg_boolean_flags_do_not_consume_pattern() {
+        for flag in [
+            "-.",
+            "--hidden",
+            "--no-hidden",
+            "-i",
+            "--ignore-case",
+            "-v",
+            "--invert-match",
+            "--no-ignore",
+            "--glob-case-insensitive",
+            "--mmap",
+            "--no-mmap",
+            "-P",
+            "--pcre2",
+            "--no-pcre2",
+            "-U",
+            "--multiline",
+            "--no-multiline",
+            "--multiline-dotall",
+            "--no-multiline-dotall",
+            "-w",
+            "--word-regexp",
+        ] {
+            let args = parse_rg![flag, "pattern"];
+            assert_eq!(args.patterns, ["pattern"]);
+            assert_eq!(args.other_args, [flag]);
+            assert_eq!(args.rg_args(), [flag, "--regexp=pattern"]);
+
+            let args = parse_rg!["pattern", flag];
+            assert_eq!(args.patterns, ["pattern"]);
+            assert_eq!(args.other_args, [flag]);
+            assert_eq!(args.rg_args(), [flag, "--regexp=pattern"]);
+        }
+    }
+
+    #[test]
+    fn rg_short_value_args_consume_one_value() {
+        for flag in [
+            "-A", "-B", "-C", "-d", "-g", "-j", "-m", "-M", "-r", "-t", "-T",
+        ] {
+            let args = parse_rg![flag, "value", "pattern"];
+            assert_eq!(args.patterns, ["pattern"], "failed for {flag}");
+            assert_eq!(args.other_args, [format!("{flag}value")]);
+
+            let args = parse_rg!["pattern", flag, "value"];
+            assert_eq!(args.patterns, ["pattern"], "failed for {flag}");
+            assert_eq!(args.other_args, [format!("{flag}value")]);
+        }
+    }
+
+    #[test]
+    fn rg_long_value_args_consume_one_value() {
+        for flag in [
+            "--after-context",
+            "--before-context",
+            "--color",
+            "--colors",
+            "--context",
+            "--context-separator",
+            "--dfa-size-limit",
+            "--engine",
+            "--field-context-separator",
+            "--field-match-separator",
+            "--glob",
+            "--hostname-bin",
+            "--hyperlink-format",
+            "--iglob",
+            "--ignore-file",
+            "--max-columns",
+            "--max-count",
+            "--max-depth",
+            "--max-filesize",
+            "--path-separator",
+            "--pre",
+            "--pre-glob",
+            "--regex-size-limit",
+            "--replace",
+            "--sort",
+            "--sortr",
+            "--threads",
+            "--type",
+            "--type-add",
+            "--type-clear",
+            "--type-not",
+        ] {
+            let args = parse_rg![flag, "value", "pattern"];
+            assert_eq!(args.patterns, ["pattern"], "failed for {flag}");
+            assert_eq!(args.other_args, [format!("{flag}=value")]);
+
+            let args = parse_rg!["pattern", flag, "value"];
+            assert_eq!(args.patterns, ["pattern"], "failed for {flag}");
+            assert_eq!(args.other_args, [format!("{flag}=value")]);
+        }
+    }
+
+    #[test]
+    fn rg_value_args_support_inline_and_flag_like_values() {
+        let args = parse_rg!["--sort=path", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["--sort=path"]);
+
+        let args = parse_rg!["--glob", "-foo", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["--glob=-foo"]);
+
+        let args = parse_rg!["-C2", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["-C2"]);
+    }
+
+    #[test]
+    fn rg_unknown_args_have_unambiguous_fallbacks() {
+        let args = parse_rg!["--future-switch", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["--future-switch"]);
+
+        let args = parse_rg!["--future-option=value", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["--future-option=value"]);
+
+        let args = parse_rg!["-in", "pattern"];
+        assert_eq!(args.patterns, ["pattern"]);
+        assert_eq!(args.other_args, ["-in"]);
+
+        let args = parse_rg!["--", "-pattern"];
+        assert_eq!(args.patterns, ["-pattern"]);
+        assert!(args.other_args.is_empty());
+    }
+
+    #[test]
+    fn rg_incompatible_output_modes_are_rejected() {
+        for flag in [
+            "-c",
+            "--count",
+            "--count-matches",
+            "-l",
+            "--files-with-matches",
+            "--files-without-match",
+            "--files",
+            "--generate=complete-bash",
+            "--pcre2-version",
+            "--type-list",
+        ] {
+            let error = try_parse_rg![flag, "pattern"].err().unwrap();
+            assert!(
+                error.to_string().contains("not supported in rgr"),
+                "unexpected error for {}: {}",
+                flag,
+                error
+            );
+        }
+    }
+
+    #[test]
     fn rg_other_args() {
         let args = parse_rg![
             "pos1",
@@ -426,7 +674,15 @@ mod tests {
         assert_eq!(args.patterns, ["pos1"]);
         assert_eq!(
             args.other_args,
-            ["pos2", "--bool", "--flag1=val1", "--flag2=val2", "-a", "-1"]
+            [
+                "pos2",
+                "--bool",
+                "--flag1=val1",
+                "--flag2",
+                "val2",
+                "-a",
+                "-1"
+            ]
         );
         assert!(!args.fixed_strings);
         assert!(args.encoding.is_none());
@@ -437,7 +693,8 @@ mod tests {
                 "pos2",
                 "--bool",
                 "--flag1=val1",
-                "--flag2=val2",
+                "--flag2",
+                "val2",
                 "-a",
                 "-1",
                 "--regexp=pos1"
@@ -472,7 +729,7 @@ mod tests {
         assert_eq!(args.rg_args(), ["--flag"]);
 
         let args = parse_rg!["--flag", "val"];
-        assert_eq!(args.rg_args(), ["--flag=val"]);
+        assert_eq!(args.rg_args(), ["--flag", "--regexp=val"]);
 
         let args = parse_rg!["--flag=val"];
         assert_eq!(args.rg_args(), ["--flag=val"]);
